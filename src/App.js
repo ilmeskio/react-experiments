40ea91ac383204fb5bd5f96be1ed34cb7bf6e8da
ilmeskio <consigliog@gmail.com>
2019-07-03 12:03:21 +0200

    use custom hooks and custom input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Input from "./Input";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faEye } from "@fortawesome/free-solid-svg-icons";
+import { useToggle } from "./customHooks";
 
 function App() {
+  const [focus, toggleFocus] = useToggle();
+  const [visible, toggleVisible] = useToggle();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Input placeholder="Username" autofocus={focus} visible={visible} />
+        <FontAwesomeIcon onClick={toggleFocus} icon={faSun} />
+        <FontAwesomeIcon onClick={toggleVisible} icon={faEye} />
       </header>
     </div>
   );
